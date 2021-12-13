e035107870f6b297bf31cd1648df03edfb8a529d
adindaajeng <adindaajengcd@gmail.com>
2021-12-14 04:40:09 +0700

    JC3-9 User Verification

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "../src/Supports/Assets/vendor/bootstrap/css/bootstrap.min.css";
 
-
 // Import pages
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import Verification from "./Pages/Verification/Verification";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
       <Switch>
         <Route component={Login} path="/login" />
         <Route component={Register} path="/register" />
+        <Route component={Verification} path="/verification/:token" />
       </Switch>
     </BrowserRouter>
   );
